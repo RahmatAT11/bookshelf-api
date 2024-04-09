b81f8cbbf863e6c490ae73465ee05d1930abf531
ralrafi12@gmail.com <ralrafi12@gmail.com>
2024-04-09 07:50:10 +0700

    change nanoid version to v3

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ const init = async () => {
   server.route(routes);
 
   await server.start();
-  // eslint-disable-next-line no-console
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
